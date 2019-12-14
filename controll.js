347222cd7ad2ba5b1869471255198774216b451f
tosaka-n <tosquitoz@gmail.com>
2019-12-14 13:37:37 +0900

    add: more function

--- controll.js
+++ controll.js
@@ -1,7 +1,7 @@
 const util = require("util")
 const exec = util.promisify(require('child_process').exec);
 
-module.exports = async function(key) {
+module.exports.irsend = async function(key) {
   console.log(`${Date().toLocaleString()} irsend ${key}`);
   const { stdout, stderr } = await exec(`irsend SEND_ONCE aircon ${key}`);
   if (stderr) {
@@ -10,3 +10,21 @@ module.exports = async function(key) {
   return `ok, ac ${stdout}`;
 }
 
+module.exports.irsendRemind = async function(key, set) {
+   const ms = {
+     h : 60 * 60 * 1000,
+     m : 60 * 1000,
+     s : 1000
+   }
+   const setms = ms[set.match(/[a-z]/)[0]];
+   const setnum = set.match(/\d{0,}/)[0];
+   if (isNaN(setms)) {
+     return "wrong setting h/m/s";
+   }
+   console.log(`${Date().toLocaleString()} irsend ${key}`);
+   const { stdout, stderr } = setTimeout(exec(`irsend SEND_ONCE aircon ${key}`), setms * setnum);
+   if (stderr) {
+     return `error: ${stderr}`;
+   }
+   return `ok, ac ${stdout}, after ${setnum} * ${setms}`;
+}
